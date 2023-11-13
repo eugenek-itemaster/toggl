550a756345ac000c5beb8c8f3c48fa43e178b17d
eugenek-itemaster <eugene.k@a-team.global>
2023-11-13 09:33:48 +0200

    Prepared for deploy

--- server/server.js
+++ server/server.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const app = express();
 const PORT = 5000;
+const path = require('path');
 
 app.use(express.json({ extended: false }));
 
@@ -8,6 +9,14 @@ app.use('/api/users', require('./routes/users'));
 app.use('/api/auth', require('./routes/auth'));
 app.use('/api/tracker', require('./routes/tracker'));
 app.use('/api/users-mysql', require('./routes/users-mysql'));
+
+if (process.env.NODE_ENV === 'production') {
+    app.use(express.static('../client/build'));
+
+    app.get('*', (req, res) => {
+        res.sendFile(path.resolve(__dirname, '../client', 'build', 'index.html'));
+    })
+}
 
 app.listen(PORT, () => {
     console.log(`Example app listening on port ${PORT}`)
