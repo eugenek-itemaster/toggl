0c90052966480b0e9953cf0d627b1c10995f8126
eugenek-itemaster <eugene.k@a-team.global>
2023-11-17 00:44:06 +0200

    env for express js

--- server/app.js
+++ server/app.js
@@ -5,9 +5,9 @@ var cors = require('cors')
 
 app.use(cors())
 
-//if (process.env.NODE_ENV !== 'production') {
+if (process.env.NODE_ENV !== 'production') {
     require('dotenv').config();
-//}
+}
 
 app.use(express.json({ extended: false }));
 
@@ -16,13 +16,13 @@ app.use('/api/auth', require('./routes/auth'));
 app.use('/api/tracker', require('./routes/tracker'));
 app.use('/api/users-mysql', require('./routes/users-mysql'));
 
-if (process.env.NODE_ENV === 'production') {
+/*if (process.env.NODE_ENV === 'production') {
     app.use(express.static('../client/build'));
 
     app.get('*', (req, res) => {
         res.sendFile(path.resolve(__dirname, '../client', 'build', 'index.html'));
     })
-}
+}*/
 
 app.listen(process.env.APP_PORT, () => {
     console.log(`Example app listening on port ${process.env.APP_PORT}`)
